684a1ec1bf6c718f55c848eb4fb263cde95c51cf
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-09-21 15:23:10 +0530

    Create ui for home screen

--- lib/home.dart
+++ lib/home.dart
@@ -72,7 +72,146 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF101010),
-      body: Container(),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: [0.002, 1],
+              colors: [Color(0xFF3A7BD5), Color(0xFF00D2FF)]),
+        ),
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                'Detect flowers',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 26),
+              ),
+              Text(
+                'By CNN with tensorflow',
+                style: TextStyle(
+                    color: Colors.white70,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600),
+              ),
+              SizedBox(height: 20),
+              Container(
+                padding: EdgeInsets.all(30),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black.withOpacity(0.4),
+                          spreadRadius: 5,
+                          blurRadius: 7),
+                    ]),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      child: Center(
+                        child: _loading
+                            ? Container(
+                                width: 300,
+                                child: Column(
+                                  children: [
+                                    Image.asset('assets/flower.png'),
+                                    SizedBox(
+                                      height: 30,
+                                    )
+                                  ],
+                                ),
+                              )
+                            : Container(
+                                child: Column(
+                                  children: <Widget>[
+                                    Container(
+                                      height: 300,
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(10),
+                                        child: Image.file(_image),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 20,
+                                    ),
+                                    _output != null
+                                        ? Text(
+                                            'Prediction is ${_output[0]['label']}',
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 20),
+                                          )
+                                        : Container(),
+                                    SizedBox(
+                                      height: 30,
+                                    )
+                                  ],
+                                ),
+                              ),
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      child: Column(
+                        children: <Widget>[
+                          GestureDetector(
+                            onTap: pickImage,
+                            child: Container(
+                              width: MediaQuery.of(context).size.width - 100,
+                              alignment: Alignment.center,
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 24, vertical: 17),
+                              decoration: BoxDecoration(
+                                color: Colors.blueAccent,
+                                borderRadius: BorderRadius.circular(6),
+                              ),
+                              child: Text(
+                                'Take a photo',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 18),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          GestureDetector(
+                            onTap: pickGalleryImage,
+                            child: Container(
+                              width: MediaQuery.of(context).size.width - 100,
+                              alignment: Alignment.center,
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 24, vertical: 17),
+                              decoration: BoxDecoration(
+                                color: Colors.blueAccent,
+                                borderRadius: BorderRadius.circular(6),
+                              ),
+                              child: Text(
+                                'Pick a photo',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 18),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
