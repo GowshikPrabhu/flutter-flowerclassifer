5652d7bc84e58601f3c901af9a80eb08547d803f
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-09-21 14:36:57 +0530

    Create splash screen

---
+++ lib/splashscreen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:jankenpon/home.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+class MySplashScreen extends StatefulWidget {
+  @override
+  _MySplashScreenState createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 2,
+      navigateAfterSeconds: Home(),
+      title: Text(
+        'Flower Identifier',
+        style: TextStyle(
+            fontWeight: FontWeight.bold, fontSize: 30, color: Colors.white),
+      ),
+      image: Image.asset('assets/flower.png'),
+      backgroundColor: Colors.black,
+      photoSize: 100,
+      loaderColor: Color(0xFF1E90FF),
+    );
+  }
+}
